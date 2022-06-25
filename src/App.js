a55e6e7523c98743b461769bc147aba6a4fee80b
Liya <theresliya@gmail.com>
2022-06-25 22:44:03 +0530

    Review changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import "./App.css";
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResults] = useState([]);
-  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  const [definition, setDefinition] = useState([]);
   const fetchWordSearchResults = async () => {
     try {
+      let dictionaryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
       const { data: response } = await axios.get(
         "https://api.datamuse.com/sug",
         { params: { s: searchTerm } }
       );
+      const { data: result } = await axios.get(dictionaryUrl);
       setSearchResults(response);
+      setDefinition(result);
     } catch (err) {
       console.log(err);
     }
@@ -22,9 +25,11 @@ function App() {
   useEffect(() => {
     if (searchTerm.trim() === "") {
       setSearchResults([]);
+      setDefinition([]);
       return;
     }
     fetchWordSearchResults();
+    console.log(definition);
   }, [searchTerm]);
 
   const onWordSelection = (word) => {
