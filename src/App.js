946b7b94ef59e43a62d18d96f5b0698b25c7fc03
Liya <theresliya@gmail.com>
2022-06-25 23:48:26 +0530

    Removed unwanted code

--- src/App.js
+++ src/App.js
@@ -7,17 +7,13 @@ import "./App.css";
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResults] = useState([]);
-  const [definition, setDefinition] = useState([]);
   const fetchWordSearchResults = async () => {
     try {
-      let dictionaryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
       const { data: response } = await axios.get(
         "https://api.datamuse.com/sug",
         { params: { s: searchTerm } }
       );
-      const { data: result } = await axios.get(dictionaryUrl);
       setSearchResults(response);
-      setDefinition(result);
     } catch (err) {
       console.log(err);
     }
@@ -25,11 +21,9 @@ function App() {
   useEffect(() => {
     if (searchTerm.trim() === "") {
       setSearchResults([]);
-      setDefinition([]);
       return;
     }
     fetchWordSearchResults();
-    console.log(definition);
   }, [searchTerm]);
 
   const onWordSelection = (word) => {
