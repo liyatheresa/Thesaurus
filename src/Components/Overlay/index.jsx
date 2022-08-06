4de9d3f5022d8d3aad492de65cc61ea31d510996
Liya <theresliya@gmail.com>
2022-08-07 03:11:07 +0530

    Added collapse inside overlay

--- src/Components/Overlay/index.jsx
+++ src/Components/Overlay/index.jsx
@@ -4,31 +4,32 @@ import { DICTIONARY_URL } from "../../constants";
 import { fetchData } from "../../util";
 import { notification } from "antd";
 import { Modal } from "antd";
+import { Collapse } from "antd";
+
+const { Panel } = Collapse;
 
 const Overlay = ({ word, setIsModalVisible, isModalVisible }) => {
   const [definitions, setDefinition] = useState([]);
-  // const [error, setError] = useState({ isSuccess: true, errorMessage: "" });
+  const [meaningsArray, setMeaningsArray] = useState([]);
 
   useEffect(() => {
     const getWordDefinition = async () => {
       const url = DICTIONARY_URL + word;
-      let arrayOfMeanings = [];
+      // let arrayOfMeanings = [];
+      let numberOfDefinitionObjects = 0;
       let { succeeded, response } = await fetchData(url);
       if (succeeded) {
-        //  setError({ ...error, isSuccess: true });
-        response.map((data) => {
-          return data.meanings.map((each) =>
-            each.definitions.map((desc) =>
-              arrayOfMeanings.push(desc.definition)
-            )
-          );
-        });
-        setDefinition(arrayOfMeanings);
-      } else {
-        // setError({
-        //   isSuccess: false,
-        //   errorMessage: response.response.data.title,
+        // response.map((data) => {
+        //   return data.meanings.map((each) =>
+        //     each.definitions.map((desc) =>
+        //       arrayOfMeanings.push(desc.definition)
+        //     )
+        //   );
         // });
+        // setDefinition(arrayOfMeanings);
+        setMeaningsArray(response);
+        console.log(meaningsArray);
+      } else {
         setIsModalVisible(false);
         notification.error({
           message: "Error",
@@ -39,7 +40,7 @@ const Overlay = ({ word, setIsModalVisible, isModalVisible }) => {
     getWordDefinition();
   }, []);
 
-  const handleOk = () => {
+  const closeModal = () => {
     setIsModalVisible(false);
   };
 
@@ -48,18 +49,35 @@ const Overlay = ({ word, setIsModalVisible, isModalVisible }) => {
       <Modal
         title={word}
         visible={isModalVisible}
-        onOk={handleOk}
+        onOk={closeModal}
+        onCancel={closeModal}
+        okText="Close"
         cancelButtonProps={{ className: "hidden" }}
+        centered
       >
-        {definitions.length !== 0 ? (
-          <ul className="list-of-meanings">
-            {definitions.map((eachMeaning) => {
-              return <li key={eachMeaning}>{eachMeaning}</li>;
-            })}
-          </ul>
-        ) : (
-          <div>Data loading...</div>
-        )}
+        <Collapse accordion>
+          {meaningsArray.map((data, index) => (
+            <Panel header={data.phonetic} key={data.phonetic + index}>
+              <Collapse ghost>
+                <Panel header="Definitions" key={"1" + data.phonetic}>
+                  <ol className="list-of-meanings">
+                    {data.meanings.map((each) =>
+                      each.definitions.map((desc) => (
+                        <li key={desc.definition}>{desc.definition}</li>
+                      ))
+                    )}
+                  </ol>
+                </Panel>
+                <Panel header="Synonyms" key={"2" + data.phonetic}>
+                  <p>Data Loading..</p>
+                </Panel>
+                <Panel header="Antonyms" key={"3" + data.phonetic}>
+                  <p>Data Loading..</p>
+                </Panel>
+              </Collapse>
+            </Panel>
+          ))}
+        </Collapse>
       </Modal>
     </>
   );
