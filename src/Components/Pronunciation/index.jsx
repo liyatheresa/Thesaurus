a77289cc51b2a95adc8ea332c65a939b3308720d
Liya <theresliya@gmail.com>
2022-08-23 00:58:44 +0530

    Review changes completed

--- src/Components/Pronunciation/index.jsx
+++ src/Components/Pronunciation/index.jsx
@@ -6,11 +6,7 @@ import "./Pronunciation.scss";
 const Pronunciation = ({ sectionData }) => {
   const getAudioPath = (sectionData) => {
     if (sectionData.audio.length > 0) {
-      if (sectionData.audio[0]) {
-        return sectionData.audio[0];
-      } else if (sectionData.audio.length > 1) {
-        return sectionData.audio[1];
-      }
+      return sectionData.audio.find((audio) => audio !== "");
     }
   };
   const playAudio = (e) => {
@@ -27,7 +23,7 @@ const Pronunciation = ({ sectionData }) => {
         shape="circle"
         icon={<SoundOutlined />}
         onClick={(e) => playAudio(e)}
-      ></Button>
+      />
     )
   );
 };
